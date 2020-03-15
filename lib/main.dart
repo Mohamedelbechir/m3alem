037a210c19b9f550f74d4e9991fdd5aae63e143b
Mohamed El Béchir <mohamedelbechir1@gmail.com>
2020-03-15 14:45:49 +0100

    ajout des block

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:m3alem/block/authentification_block.dart';
+import 'package:m3alem/block/authentification_event.dart';
+import 'package:m3alem/block/authentification_state.dart';
+import 'package:m3alem/pages/home.dart';
+import 'package:m3alem/pages/login_page.dart';
+import 'package:m3alem/repository/utilisateur_repository.dart';
 
-void main() => runApp(MyApp());
+import 'widgets/loading_indicator.dart';
+import 'package:bloc/bloc.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      body: Center(
+        child: Text('Splash Screen'),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SimpleBlocDelegate extends BlocDelegate {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void onEvent(Bloc bloc, Object event) {
+    super.onEvent(bloc, event);
+    print(event);
   }
 
   @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
+    super.onError(bloc, error, stacktrace);
+    print(error);
+  }
+}
+
+void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  final utilisateurRepository = UtilisateurRepository();
+  runApp(
+    BlocProvider<AuthentificationBloc>(
+      
+      builder: (context) {
+        return AuthentificationBloc(utilisateurRepository: utilisateurRepository)
+          ..dispatch(AppStarted());
+      },
+      /* A chaque fois qu'un évement est mis alors 
+          il dit aux nœuds dependant de se reconstruire
+      */
+      child: MyApp(utilisateurRepository: utilisateurRepository),
+    ),
+  );
+  //runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final UtilisateurRepository utilisateurRepository;
+
+  MyApp({Key key, @required this.utilisateurRepository}) : super(key: key);
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      checkerboardOffscreenLayers: true,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+       
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      /*  builder: (context, widget) => MultiProvider(
+        providers: [
+          Provider<RdvRepositorySql>.value(value: RdvRepositorySql()),
+          Provider<PatientRepositorySql>.value(value: PatientRepositorySql()),
+          Provider<RdvStatusRepositorySql>.value(
+              value: RdvStatusRepositorySql()),
+          Provider<RdvTypeRepositorySql>.value(value: RdvTypeRepositorySql()),
+          Provider<RdvRepository>.value(value: RdvRepository()),
+        ],
+        child: widget,
+      ), */
+      builder: (context, widget) => MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider<UtilisateurRepository>(
+            builder: (context) => UtilisateurRepository(),
+          ),
+         /*  RepositoryProvider<PatientRepositorySql>(
+            builder: (context) => PatientRepositorySql(),
+          ), */
+          
+          
+        ],
+        child: widget,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: BlocBuilder<AuthentificationBloc, AuthentificationState>(
+        builder: (context, state) {
+          if (state is AuthentificationAuthenticated) {
+            //return HomePage();
+            return HomePage();
+          }
+          if (state is AuthentificationUnauthenticated) {
+            return LoginPage(utilisateurRepository: utilisateurRepository);
+          }
+          if (state is AuthentificationLoading) {
+            return LoadingIndicator();
+          }
+          return SplashPage();
+        },
+      ),
+      
     );
   }
 }
